fcb2b020ed92e956f915835477774bbee62034bb
Hugh Winkler <hwinkler@drillinginfo.com>
2015-04-29 16:22:54 -0500

    Remove zlib from build and quiet down the closure compiler

--- build.sh
+++ build.sh
@@ -1,36 +1,46 @@
 #!/bin/bash
 
 export EMCC_CFLAGS="-O2"
-ZLIB_PKGVER=1.2.8
+#ZLIB_PKGVER=1.2.8
 LIBTIFF_PKGVER=4.0.3
-LIBZ_STATIC=
+#LIBZ_STATIC=
+MUST_PATCH=0
 OS=`uname`
 
 if [ $OS == 'Darwin' ]
     then
         export LLVM=/usr/local/opt/emscripten/libexec/llvm/bin
-        LIBZ_STATIC=--static
-        echo setting staitc flag for libz: $LIBZ_STATIC
+        #LIBZ_STATIC=--static
+        #echo setting staitc flag for libz: $LIBZ_STATIC
 fi
 
 # check prerequisite: tsc
 command -v tsc >/dev/null 2>&1 || { echo >&2 "Build requires the tsc command (Typescript) but it's not installed.  Aborting."; exit 1; }
 
 # build zlib
-wget http://zlib.net/current/zlib-${ZLIB_PKGVER}.tar.gz
-tar xf zlib-${ZLIB_PKGVER}.tar.gz
-cd zlib-${ZLIB_PKGVER}
-emconfigure ./configure $LIBZ_STATIC
-emmake make
-cd ..
+# wget http://zlib.net/current/zlib-${ZLIB_PKGVER}.tar.gz
+# tar xf zlib-${ZLIB_PKGVER}.tar.gz
+# cd zlib-${ZLIB_PKGVER}
+# emconfigure ./configure $LIBZ_STATIC
+# emmake make
+# cd ..
 
 # build libtiff
-wget http://download.osgeo.org/libtiff/tiff-${LIBTIFF_PKGVER}.tar.gz
-tar xzvf tiff-${LIBTIFF_PKGVER}.tar.gz
+if [ ! -d tiff-${LIBTIFF_PKGVER} ];
+    then
+        wget http://download.osgeo.org/libtiff/tiff-${LIBTIFF_PKGVER}.tar.gz
+        tar xzvf tiff-${LIBTIFF_PKGVER}.tar.gz
+        MUST_PATCH=1
+fi
+
 cd tiff-${LIBTIFF_PKGVER}
-# see: https://github.com/kripken/emscripten/issues/662
-patch -p0 < ../tif_open.c.patch
-patch -p0 < ../tiff.h.patch
+
+if [ MUST_PATCH == 1 ]; then
+    # see: https://github.com/kripken/emscripten/issues/662
+    patch -p0 < ../tif_open.c.patch
+    patch -p0 < ../tiff.h.patch
+fi
+
 emconfigure ./configure --enable-shared
 emmake make
 cd ..
@@ -53,8 +63,8 @@ emcc -o tiff.raw.js \
 "'_GetField',"\
 "'FS']" \
     export.c \
-    tiff-${LIBTIFF_PKGVER}/libtiff/.libs/libtiff.a \
-    zlib-${ZLIB_PKGVER}/libz.a
+    tiff-${LIBTIFF_PKGVER}/libtiff/.libs/libtiff.a 
+    #zlib-${ZLIB_PKGVER}/libz.a
 
 echo 'var TiffTag = {' > tiff_tag.ts
 grep '^#define[[:space:]]\+TIFFTAG_[A-Za-z_]\+[[:space:]]\+' \
@@ -71,6 +81,7 @@ mv tiff_api.d.ts tiff.d.ts
 rm -f tiff_tag.d.ts tiff_tag.js tiff_api.js
 
 closure-compiler \
+    --warning_level=QUIET \
     --js=tiff.js \
     --js_output_file=tiff.min.js \
     --language_in ECMASCRIPT5 \
